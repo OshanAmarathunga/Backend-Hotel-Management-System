72e4fce8079cca27332fb2baf1438e5d16d8d2eb
Oshan Amarathunga <156085520+OshanAmarathunga@users.noreply.github.com>
2024-10-12 19:35:43 +0530

    Added delete fucntion for category

--- routes/CategoryRoute.js
+++ routes/CategoryRoute.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { getAllCategories, saveCategory } from '../controllers/CategoryController.js';
+import { deleteCategory, getAllCategories, getCategoryByName, saveCategory } from '../controllers/CategoryController.js';
 
 const categoryRouter=express.Router();
 
 categoryRouter.get('/',getAllCategories);
 categoryRouter.post('/',saveCategory); 
+categoryRouter.delete('/:categoryName',deleteCategory)
+categoryRouter.get('/:requiredName',getCategoryByName)
 
 export default categoryRouter;
