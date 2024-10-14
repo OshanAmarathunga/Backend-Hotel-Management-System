6cdd74bb98013f328fae8ac45ccee2b9a0fef015
Oshan Amarathunga <156085520+OshanAmarathunga@users.noreply.github.com>
2024-10-14 09:14:16 +0530

    Added Update fucntion to category.js and updated the authnetication function

--- routes/CategoryRoute.js
+++ routes/CategoryRoute.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { deleteCategory, getAllCategories, getCategoryByName, saveCategory } from '../controllers/CategoryController.js';
+import { deleteCategory, getAllCategories, getCategoryByName, saveCategory, updateCategory } from '../controllers/CategoryController.js';
 
 const categoryRouter=express.Router();
 
 categoryRouter.get('/',getAllCategories);
 categoryRouter.post('/',saveCategory); 
-categoryRouter.delete('/:categoryName',deleteCategory)
-categoryRouter.get('/:requiredName',getCategoryByName)
+categoryRouter.delete('/:categoryName',deleteCategory);
+categoryRouter.get('/:requiredName',getCategoryByName);
+categoryRouter.put('/:categoryName',updateCategory);
 
 export default categoryRouter;
