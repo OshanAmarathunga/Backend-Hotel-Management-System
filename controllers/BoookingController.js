3e2479ebe9b26120701049be60c49d7d6b1a6108
Oshan Amarathunga <156085520+OshanAmarathunga@users.noreply.github.com>
2024-10-15 16:20:10 +0530

    added validation for bookings

--- controllers/BoookingController.js
+++ controllers/BoookingController.js
@@ -1,86 +1,137 @@
 import Booking from "../models/Booking.js";
 import Room from "../models/Room.js";
 
-export function createBooking(req,res){
-    
+export function createBooking(req, res) {
+  if (req.user == null) {
+    res.json({
+      message: "Please login to create booking!",
+    });
+    return;
+  }
+  if (req.user.type != "customer") {
+    res.json({
+      message: "You are not a customer to create booking",
+    });
+    return;
+  }
 
-    if(req.user==null){
+  const startDate = req.body.start;
+  const endDate = req.body.end;
+  const roomId = req.body.roomId;
+
+  Booking.find({
+    roomId: roomId,
+    $or: [{ start: { $lt: endDate }, end: { $gt: startDate } }],
+  })
+    .then((result) => {
+      if (result.length > 0) {
         res.json({
-            message:"Please login to create booking!"
+          message: "Room is not available for the selected dates.",
         });
-        return
-    }
-    if(req.user.type!="customer"){
-        res.json({
-            message:"You are not a customer to create booking"
-        });
-        return
-    }
+      } else {
+        var startingID = 1000;
+        Booking.countDocuments()
+          .then((count) => {
+            const newID = startingID + count + 1;
+            const newBooking = new Booking({
+              bookingId: newID,
+              roomId: req.body.roomId,
+              email: req.user.email,
+              start: req.body.start,
+              end: req.body.end,
+            });
 
-    var startingID=1000;
-    Booking.countDocuments().then((count)=>{
-       const newID=startingID+count+1;
-       const newBooking=new Booking({
-        bookingId:newID,
-        roomId:req.body.roomId,
-        email:req.user.email,
-        start:req.body.start,
-        end:req.body.end,
-
-       });
-
-       newBooking.save().then((createdBooking)=>{
-        res.json({
-            createdBooking
-        })
-       }).catch((e)=>{
-        res.json({
-            e
-        })
-       })
-    }).catch((e)=>{
-        res.json({
-            message:"booking number error!"
-        })
+            newBooking
+              .save()
+              .then((createdBooking) => {
+                res.json({
+                  createdBooking,
+                });
+              })
+              .catch((e) => {
+                res.json({
+                  e,
+                });
+              });
+          })
+          .catch((e) => {
+            res.json({
+              message: "booking number error!",
+            });
+          });
+      }
     })
+    .catch((e) => {});
+
+  //   var startingID = 1000;
+  //   Booking.countDocuments()
+  //     .then((count) => {
+  //       const newID = startingID + count + 1;
+  //       const newBooking = new Booking({
+  //         bookingId: newID,
+  //         roomId: req.body.roomId,
+  //         email: req.user.email,
+  //         start: req.body.start,
+  //         end: req.body.end,
+  //       });
+
+  //       newBooking
+  //         .save()
+  //         .then((createdBooking) => {
+  //           res.json({
+  //             createdBooking,
+  //           });
+  //         })
+  //         .catch((e) => {
+  //           res.json({
+  //             e,
+  //           });
+  //         });
+  //     })
+  //     .catch((e) => {
+  //       res.json({
+  //         message: "booking number error!",
+  //       });
+  //     });
 }
 
-export function getBookingsBasedOnUserRole(req,res){
-    if(req.user==null){
-        res.json({
-            message:"Please login to create booking!"
-        });
-        return
-    }
+export function getBookingsBasedOnUserRole(req, res) {
+  if (req.user == null) {
+    res.json({
+      message: "Please login to create booking!",
+    });
+    return;
+  }
 
-    if(req.user.type=="customer"){
-        const userEmail=req.user.email;
+  if (req.user.type == "customer") {
+    const userEmail = req.user.email;
 
-        Booking.find({email:userEmail}).then((bookings)=>{
-            res.json({
-                bookings
-            })
-        })
-    }else{
-        Booking.find().then((AllBookings)=>{
-            res.json({
-                AllBookings
-            })
-        })
-    }
+    Booking.find({ email: userEmail }).then((bookings) => {
+      res.json({
+        bookings,
+      });
+    });
+  } else {
+    Booking.find().then((AllBookings) => {
+      res.json({
+        AllBookings,
+      });
+    });
+  }
 }
 
+export function updatBooking(req, res) {
+  const bookingId = req.params.bookingId;
 
-export function updatBooking(req,res){
-    const bookingId=req.params.bookingId;
-    
-    Booking.updateOne({bookingId:bookingId},req.body).then((booking)=>{
-        res.json({
-            booking
-        })
-    }).catch((e)=>{
-        res.json({
-            e
-        })
+  Booking.updateOne({ bookingId: bookingId }, req.body)
+    .then((booking) => {
+      res.json({
+        booking,
+      });
     })
+    .catch((e) => {
+      res.json({
+        e,
+      });
+    });
 }
