8ae72e609e6d2809ca1b108cc48223b592481695
Oshan Amarathunga <156085520+OshanAmarathunga@users.noreply.github.com>
2024-10-27 13:52:43 +0530

    fixed some issues in API end points

--- models/Category.js
+++ models/Category.js
@@ -1,27 +1,27 @@
-import mongoose from "mongoose";
+    import mongoose from "mongoose";
 
-const categorySchema=mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
-        unique:true
-    },
-    description:{
-        type:String 
-    },
-    price:{
-        type:Number,  
-        required:true 
-    },
-    features:[
-        {
+    const categorySchema=mongoose.Schema({
+        name:{
+            type:String,
+            required:true,
+            unique:true
+        },
+        description:{
+            type:String 
+        },
+        price:{
+            type:Number,  
+            required:true 
+        },
+        features:[
+            {
+                type:String
+            }
+        ],
+        image:{
             type:String
-        }
-    ],
-    image:{
-        type:String
-    }    
-})
+        }    
+    })
 
-const category=mongoose.model("Category",categorySchema);
-export default category;
+    const category=mongoose.model("Category",categorySchema);
+    export default category;
