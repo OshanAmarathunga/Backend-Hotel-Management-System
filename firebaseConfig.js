323c7e8cec742887c0bcc41c11befd2a965611c0
Oshan Amarathunga <156085520+OshanAmarathunga@users.noreply.github.com>
2024-11-07 16:05:27 +0530

    bugfixed

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,8 +1,23 @@
 import admin from 'firebase-admin';
-import serviceAccount from './serviceAccount.json' assert { type: 'json' };
+import dotenv from "dotenv";
+dotenv.config();
+
+const serviceAcount={
+  "type": process.env.TYPE,
+  "project_id": process.env.PROJECT_ID, 
+  "private_key_id":process.env.PRIVATE_KEY_ID,
+  "private_key": process.env.PRIVATE_KEY,
+  "client_email": process.env.CLIENT_EMAIL,
+  "client_id": process.env.CLIENT_ID,
+  "auth_uri": process.env.AUTH_URI,
+  "token_uri": process.env.TOKEN_URI,
+  "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER,
+  "client_x509_cert_url": process.env.CLIENT,
+  "universe_domain": process.env.UNIVERSE_DOMAIN
+}
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAcount)
 });
 
-export default admin; 
+export default admin;
