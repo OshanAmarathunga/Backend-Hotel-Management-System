7c982850e793e5b8837da11c6362600d3b188add
Oshan Amarathunga <156085520+OshanAmarathunga@users.noreply.github.com>
2024-11-26 17:25:13 +0530

    feedback

--- controllers/FeedBackController.js
+++ controllers/FeedBackController.js
@@ -1,46 +1,93 @@
 import Feedback from "../models/Feedback.js";
 
+export function saveFeedback(req, res) {
+  const feedback = req.body;
 
-export function saveFeedback(req,res){
-    const feedback=req.body;
-    
-    const newFeedback=new Feedback(feedback);
-    newFeedback.save().then((rsp)=>{
-        
-        res.status(200).json({
-            
-            
-            message:"Feedback Saved !"
-        })
-    }).catch((e)=>{
-        console.log(e);
-        
-        res.status(400).json({
-            message:"Failed to save"
-        })
+  const newFeedback = new Feedback(feedback);
+  newFeedback
+    .save()
+    .then((rsp) => {
+      res.status(200).json({
+        message: "Feedback Saved !",
+      });
     })
+    .catch((e) => {
+      console.log(e);
+
+      res.status(400).json({
+        message: "Failed to save",
+      });
+    });
 }
 
-export function getAllFeedbacks(req,res){
-    Feedback.find().then((rsp)=>{
-        res.status(200).json({
-            allFeedBacks:rsp
-        })
-    }).catch((e)=>{
-        res.status(400).json({
-            e
-        })
+export function getAllFeedbacks(req, res) {
+  Feedback.find()
+    .then((rsp) => {
+      res.status(200).json({
+        allFeedBacks: rsp,
+      });
     })
+    .catch((e) => {
+      res.status(400).json({
+        e,
+      });
+    });
 }
 
-export function getApprovedFeedbacks(req,res){
-    Feedback.find({status:"Approved"}).then((rsp)=>{
-        res.status(200).json({
-            ApprovedFeedbacks:rsp
-        })
-    }).catch((e)=>{
-        res.status(400).json({
-            e
-        })
+export function getApprovedFeedbacks(req, res) {
+  Feedback.find({ status: "Approved" })
+    .then((rsp) => {
+      res.status(200).json({
+        ApprovedFeedbacks: rsp,
+      });
     })
+    .catch((e) => {
+      res.status(400).json({
+        e,
+      });
+    });
+}
+
+export function deleteFeedback(req, res) {
+  const id = req.params.id;
+  Feedback.findByIdAndDelete(id)
+    .then((rsp) => {
+      res.status(200).json({
+        message: "Feedback Deleted",
+      });
+    })
+    .catch((e) => {
+      res.status(400).json({
+        error: e,
+      });
+    });
+}
+
+export function updateStatus(req, res) {
+  const id = req.params.id;
+
+  Feedback.findById(id)
+    .then((rsp) => {
+      let newStatus = "";
+      if (rsp.status == "pending") {
+        newStatus = "approved";
+      } else {
+        newStatus = "pending";
+      }
+
+      rsp.status = newStatus;
+
+      Feedback.updateOne({ _id: id }, rsp)
+        .then((result) => {
+          res.status(200).json({
+            message: "updated",
+          });
+        })
+        .catch((e) => {
+          res.status(400).json({
+            error: e,
+          });
+        });
+    })
+    .catch((e) => {});
 }
--- routes/FeedbackRoute.js
+++ routes/FeedbackRoute.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { saveCategory } from '../controllers/CategoryController.js';
-import { getAllFeedbacks, getApprovedFeedbacks, saveFeedback } from '../controllers/FeedBackController.js';
+import { deleteFeedback, getAllFeedbacks, getApprovedFeedbacks, saveFeedback, updateStatus } from '../controllers/FeedBackController.js';
 
 const feedbackRouter=express.Router();
 
 feedbackRouter.post('/',saveFeedback );
 feedbackRouter.get('/getAll',getAllFeedbacks );
 feedbackRouter.get('/getApprovedFeedbacks', getApprovedFeedbacks);
+feedbackRouter.delete('/:id',deleteFeedback );
+feedbackRouter.put('/:id', updateStatus);
 
 export default feedbackRouter;   
