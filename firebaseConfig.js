3ce5dc060a27fc64397a45d8f5fbf99e4d59e8c1
Oshan Amarathunga <156085520+OshanAmarathunga@users.noreply.github.com>
2024-12-01 16:27:18 +0530

    new

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,23 +1,45 @@
-import admin from 'firebase-admin';
+import admin from "firebase-admin";
 import dotenv from "dotenv";
+
 dotenv.config();
 
-const serviceAcount={
-  "type": process.env.TYPE,
-  "project_id": process.env.PROJECT_ID, 
-  "private_key_id":process.env.PRIVATE_KEY_ID,
-  "private_key": process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
-  "client_email": process.env.CLIENT_EMAIL,
-  "client_id": process.env.CLIENT_ID,
-  "auth_uri": process.env.AUTH_URI,
-  "token_uri": process.env.TOKEN_URI,
-  "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER,
-  "client_x509_cert_url": process.env.CLIENT,
-  "universe_domain": process.env.UNIVERSE_DOMAIN  
-}
-//firebase
+const requiredEnvVariables = [
+  "TYPE",
+  "PROJECT_ID",
+  "PRIVATE_KEY_ID",
+  "PRIVATE_KEY",
+  "CLIENT_EMAIL",
+  "CLIENT_ID",
+  "AUTH_URI",
+  "TOKEN_URI",
+  "AUTH_PROVIDER",
+  "CLIENT",
+  "UNIVERSE_DOMAIN",
+];
+
+requiredEnvVariables.forEach((key) => {
+  if (!process.env[key]) {
+    throw new Error(`Environment variable ${key} is missing`);
+  }
+});
+
+const serviceAccount = {
+  type: process.env.TYPE,
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: process.env.AUTH_URI,
+  token_uri: process.env.TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER,
+  client_x509_cert_url: process.env.CLIENT,
+  universe_domain: process.env.UNIVERSE_DOMAIN,
+};
+
+// Initialize Firebase Admin SDK
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAcount)
+  credential: admin.credential.cert(serviceAccount),
 });
 
 export default admin;
